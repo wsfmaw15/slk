69db4d0c99d97fa90abd0412ee6eaa6a03b2086a
wsfmaw15 <127457500+wsfmaw15@users.noreply.github.com>
2024-11-27 21:00:10 -0300

    Add files via upload

---
+++ graficos/quantidadeUsuarios.js
@@ -0,0 +1,61 @@
+import { getCSS, tickConfig } from "./common.js"
+
+async function quantidadeUsuariosPorRede() {
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
+    const res = await fetch(url)
+    const dados = await res.json()
+    const nomeDasRedes = Object.keys(dados)
+    const quantidadeDeUsuarios = Object.values(dados)
+
+    const data = [        
+        {
+            x: nomeDasRedes, 
+            y: quantidadeDeUsuarios, 
+            type: 'bar',
+            marker: {
+                color: getCSS('--primary-color')
+            }
+        }
+    ]
+
+    const layout = {
+        plot_bgcolor: getCSS('--bg-color'),
+        paper_bgcolor: getCSS('--bg-color'),
+        titlte: {
+            text: 'Redes sociais com mais usuarios no mundo',
+            x: 0,
+            font: {
+                color: getCSS('--primary-color'),
+                family: getCSS('--font'),
+                size: 30
+            }
+        },
+        xaxis: {
+            tickfont: tickConfig,
+             titlte: {
+                text: 'nome das redes sociais',
+                font: {
+                    color: getCSS('--secondary-color')
+                }
+             }
+        },
+        yaxis: {
+            tickfont: tickConfig,
+            titlte: {
+                text: 'bilhoes de usuarios ativos',
+                font: {
+                    color: getCSS('--secondary-color')
+                }
+             }
+        }
+
+
+    }
+
+    const grafico = document.createElement('div')
+    grafico.className = 'grafico'
+    document.getElementById('graficos-container').appendChild(grafico)
+    Plotly.newPlot(grafico, data, layout)
+}
+
+quantidadeUsuariosPorRede()
